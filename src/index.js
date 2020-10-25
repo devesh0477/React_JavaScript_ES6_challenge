bb116e75077003f607b801fdce510d15170ed9e6
devesh0477 <55842034+devesh0477@users.noreply.github.com>
2020-10-25 02:15:04 -0400

    solution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import ReactDOM from "react-dom";
 //from the calculator.js file.
 //If successful, your website should look the same as the Final.png
 
+// -----------------------------------------------------------------------------------
+//                        Solution :  Method 1
+// -----------------------------------------------------------------------------------
+// import * as operations from "./calculator";
+// ReactDOM.render(
+//   <ul>
+//     <li>{operations.add(1, 2)}</li>
+//     <li>{operations.multiply(2, 3)}</li>
+//     <li>{operations.subtract(7, 2)}</li>
+//     <li>{operations.divide(5, 2)}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                        Solution :  Method 2
+// -----------------------------------------------------------------------------------
+
+import { add, multiply, subtract, divide } from "./calculator";
 ReactDOM.render(
   <ul>
     <li>{add(1, 2)}</li>
